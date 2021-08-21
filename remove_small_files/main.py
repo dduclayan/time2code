4c1c8f7d3dfad6acb3fe5445086db5a8dc7d4ebd
Devin Duclayan <dduclayan@gmail.com>
2021-08-21 12:30:33 -0500

    Update main.py

--- remove_small_files/main.py
+++ remove_small_files/main.py
@@ -1,30 +1,37 @@
-"""
-idea:
-1) list test_dir to get all files
-2) check the size of each file and remove whose size are < 3KB
+"""TODO(dduclayan): DO NOT SUBMIT without one-line documentation for rm_small_files.
 
-complexity: O(N) where N is num of files
+remove files less than 3Kb
+
+TODO(dduclayan): DO NOT SUBMIT without a detailed description of rm_small_files.
 """
 
 import os
-from absl import app
+import sys
 
-def main(argv):
-    rm_small_files(argv[1])
+
+def list_files_and_size(dir):
+  for root, _, files in os.walk(dir):
+    for file in sorted(files):
+      file_size = os.path.getsize(root + file)
+
+      print(f'{file} {file_size}KB')
+
 
 def rm_small_files(dir):
-    files = os.listdir(dir)
-    rm_files = set()
-    print(dir, '/')
+  for root, _, files in os.walk(dir):
     for file in sorted(files):
-        size = os.stat(dir + '/' + file).st_size / 1024
-        print('{} {:.1f}KB'.format(file, size)) 
-        if size < 3:
-            rm_files.add(file)
-    print()
-    for file in rm_files:
-        print(file, 'removed!')
-        os.remove(dir + '/' + file)
+      file_size = os.path.getsize(root + file)
+
+      if file_size < 3000:
+        print(f'removing {root + file}!')
+        os.remove(root + file)
+
+
+def main():
+  small_file_dir = sys.argv[1]
+  list_files_and_size(small_file_dir)
+  print()
+  rm_small_files(small_file_dir)
 
 if __name__ == '__main__':
-    app.run(main)
+  main()
