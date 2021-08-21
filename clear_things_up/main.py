b8ce59af00fed33b3de796a1328963619ac32371
Devin Duclayan <dduclayan@gmail.com>
2021-08-21 11:56:34 -0500

    Update main.py

--- clear_things_up/main.py
+++ clear_things_up/main.py
@@ -1,36 +1,46 @@
 """
-idea:
-1) use dict (pid -> [start, end]) to store time spent
-2) calculate time spent for each pid and store (pid, time spent) to a list
-3) sort the list by pid and print them
 
-complexity: O(NlogN) where N is number of lines
+plan:
+1. add pid: start time, end time to d
+2. calculate time spent and add it to list
+3. sort list by pid
+
 """
 
-from absl import app
+import sys
 from datetime import datetime
 
-def main(argv):
-    clear_up(argv[1])
 
-def clear_up(file):
-    dic = {}
-    with open(file, 'r') as f:
-        f.readline()
-        for line in f:
-            words = line.split()
-            if words[2] not in dic:
-                dic[words[2]] = []
-            dic[words[2]].append(words[0] + ' ' + words[1])
-        for pid, t in sorted(dic.items(), key=lambda i:i[0]):
-            print(pid + ' ' + cal_time(t[0], t[1]))
-    return
+def clear_things_up(file):
+  d = {}
+  with open(file, 'r') as in_file:
+    first_line = in_file.readline()
+    for line in in_file:
+      words = line.split()
+      pid = words[2]
+      time = words[0]
+      day = words[1]
 
-def cal_time(a, b):
-    ta = datetime.strptime(a, '%H:%M:%S %m/%d/%Y')
-    tb = datetime.strptime(b, '%H:%M:%S %m/%d/%Y')
-    d = tb - ta
-    return str(d)
+      if pid not in d:
+        d[pid] = []
+      if pid in d:
+        d[pid] += [time + ' ' + day]
+
+  for pid, times in sorted(d.items(), key=lambda i: i[0]):
+    print(pid + ' ' + calc_time(times[0], times[1]))
+
+
+def calc_time(start, end):
+  a = datetime.strptime(end, '%H:%M:%S %m/%d/%Y')
+  b = datetime.strptime(start, '%H:%M:%S %m/%d/%Y')
+  c = a - b
+  return str(c)
+
+
+def main():
+  file = sys.argv[1]
+  clear_things_up(file)
+
 
 if __name__ == '__main__':
-    app.run(main)
+  main()
