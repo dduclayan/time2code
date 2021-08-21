2fe7467f9c3fc7ca877ed40d91384967f45e9a28
Devin Duclayan <dduclayan@gmail.com>
2021-08-21 12:14:42 -0500

    Update main.py

--- find_disk_full/main.py
+++ find_disk_full/main.py
@@ -1,34 +1,43 @@
+"""TODO(dduclayan): DO NOT SUBMIT without one-line documentation for find_disk_full_servers.
+
+Plan:
+
+1. Turn file into a list
+2. Loop through lines
+3. Split line
+4. Delete words we don't need
+5. Check if last element is > 85 (also need to account for 85.x)
+6. Write element 1 and last element to file
+
+TODO(dduclayan): DO NOT SUBMIT without a detailed description of find_disk_full_servers.
 """
-idea:
-1) read each line and get the number (int/float)
-2) if number > 85, add server name and percentage to a list
-3) print the list
-
-Complexity: O(N) where N is # of lines 
-"""
+import sys
 
 
-from absl import app
+def find_full_disk(file):
+  disk_list = []
+  full_disk_list = []
 
-def main(argv):
-    find_disk_full(argv[1])
-    return
+  with open(file, 'r') as in_file:
+    for line in in_file:
+      disk_list.append(line)
 
-def find_disk_full(file):
-    li = []
-    with open(file, 'r') as fd:
-        for line in fd:
-            words = line.split()
-            num = float(words[-1].rstrip('%'))
-            if num > 85:
-                if num.is_integer():
-                    li.append(words[0] + ', ' + str(int(num)) + '%')
-                else:
-                    li.append(words[0] + ', ' + str(num) + '%')
-    for i in li:
-        print(i)
-    return
+  for li in disk_list:
+    words = li.split()
+    del words[1:3]
+
+    if float(words[-1].rstrip('%')) > 85:
+      full_disk_list.append(words)
+
+  with open('full_disk_output.txt', 'w') as out_file:
+    for line in full_disk_list:
+      out_file.write('%s\n' % line)
+
+
+def main():
+  file = sys.argv[1]
+  find_full_disk(file)
 
 
 if __name__ == '__main__':
-    app.run(main)
+  main()
