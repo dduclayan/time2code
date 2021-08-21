76225938c32070e4a4117b1352da619a05f3490a
Devin Duclayan <dduclayan@gmail.com>
2021-08-21 12:28:55 -0500

    Update main.py

--- qps_per_server/main.py
+++ qps_per_server/main.py
@@ -1,35 +1,63 @@
+"""TODO(dduclayan): DO NOT SUBMIT without one-line documentation for qps_per_server.
+
+plan:
+1. store in dict - [(server_id, time)]: count
+ex.
+[(server1, 5:05:03)]: 2, [(server2, 5:05:03)]: 2
+
+for server, time, count in dict.items():
+  print(f'{server}  {QPS}')
+
+Clarifying questions:
+1. Do we want to return an int or float?
+2. Are query_IDs unique?
+
+
+TODO(dduclayan): DO NOT SUBMIT without a detailed description of qps_per_server.
 """
-idea:
-1) create a dict {server -> counter} to store queries
-2) keep a set to keep track how many seconds are in total
-3) get len of set to get total seconds
-4) calcult qps per server and output
+import sys
 
-complexity: O(NlogN) where N is number lines
-"""
 
-from absl import app
+def find_qps(file):
+  with open(file, 'r') as in_file:
+    first_line = in_file.readline()
+    server_count = {}
+    server1_count = 0
+    server2_count = 0
+    server3_count = 0
+    amt_of_secs = set()
 
-def main(argv):
-    qps_per_server(argv[1])
+    for line in in_file:
+      segment = line.split()
+      time = segment[0]
+      server = segment[2]
 
-def qps_per_server(log):
-    servers = dict()
-    sec = set()
-    with open(log, 'r') as f:
-        f.readline()
-        for line in f:
-            words = line.split()
-            sec.add(words[0])
-            if words[2] not in servers:
-                servers[words[2]] = 0
-            servers[words[2]] += 1
-    total_sec = len(sec)
-    sorted_servers = sorted(servers.items(), key=lambda i:i[0])
+      if time not in amt_of_secs:
+        amt_of_secs.add(time)
+
+      if (server, time) in server_count:
+        server_count[(server, time)] += 1
+      else:
+        server_count[(server, time)] = 1
+
+    for (server, time), count in server_count.items():
+      if 'server1' in (server, time):
+        server1_count += count
+      if 'server2' in (server, time):
+        server2_count += count
+      if 'server3' in (server, time):
+        server3_count += count
+
     print('Server_ID QPS')
-    for s,c in sorted_servers:
-        print('{0} {1:.1f}'.format(s, c/total_sec))
+    print(f'Server1 {round(server1_count/len(amt_of_secs))}')
+    print(f'Server2 {round(server2_count/len(amt_of_secs))}')
+    print(f'Server3 {round(server3_count/len(amt_of_secs))}')
+
+
+def main():
+  file = sys.argv[1]
+  find_qps(file)
+
 
 if __name__ == '__main__':
-    app.run(main)
-    
+  main()
