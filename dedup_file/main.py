b06f6822f592277306922dbf32e680cc7a86fe8c
Devin Duclayan <dduclayan@gmail.com>
2021-08-21 12:08:53 -0500

    Update main.py

--- dedup_file/main.py
+++ dedup_file/main.py
@@ -1,29 +1,41 @@
+"""TODO(dduclayan): DO NOT SUBMIT without one-line documentation for dedup_file.
+
+plan:
+1. add lines to a list
+2. loop through list and add them to a dict to remove dupes
+3. write key(line) to file
+
+can't use set. sets are unordered and you can't run sorted() on them.
+could use ordered dict
+
+TODO(dduclayan): DO NOT SUBMIT without a detailed description of dedup_file.
 """
-idea:
-1) create empty list
-2) read each line and check if it's in the list 
-      yes, drop it
-      no, append it to a list
-3) output the new file
 
-Complexity: O(N^2) where N is # of lines
-"""
+import sys
 
-
-from absl import app
-
-def main(argv):
-    dedup(argv[1])
 
 def dedup(file):
-    li = []
-    with open(file, 'r') as fd:
-        for line in fd:
-            if line not in li:
-                li.append(line)
-    with open('newfile', 'w') as fd:
-        fd.writelines(li)
-    return
+  """Turns the lines of a file into a dict, then write dict to file."""
+  file_lines = []
+  file_dict = {}
+
+  with open(file, 'r') as in_file:
+    file = [line.rstrip() for line in in_file]
+    for li in file:  # O(n)
+      file_lines.append(li)
+
+  for line in file_lines:  # O(n)
+    file_dict[line] = 1
+
+  with open('new_file1.txt', 'w') as out_file:
+    for key in file_dict:  # O(n)
+      out_file.write(key + '\n')
+
+
+def main():
+  file = sys.argv[1]
+  dedup(file)
+
 
 if __name__ == '__main__':
-    app.run(main)
+  main()
