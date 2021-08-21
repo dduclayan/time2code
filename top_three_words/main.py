650a1e808686d04255161a52597df3fe94e5d778
Devin Duclayan <dduclayan@gmail.com>
2021-08-21 12:39:05 -0500

    Update main.py

--- top_three_words/main.py
+++ top_three_words/main.py
@@ -1,29 +1,73 @@
-"""
-idea:
-1) create a dict { word -> count }
-2) read each line and split it to words and add them to dict
-3) print out the top 3 words from dict, and continue to check if the next word has the same count, if so continue to print out words until seeing a smaller count
+"""TODO(dduclayan): DO NOT SUBMIT without one-line documentation for top_ten_words.
 
-complexity: O(NlogN) where N is number total words
+plan:
+1. use dict. ex. word -> count
+2. write dict.items() to list
+3. sort list by count and return top ten
+
+TODO(dduclayan): DO NOT SUBMIT without a detailed description of top_ten_words.
 """
 
 import sys
-from collections import defaultdict
 
-def top_three(filename):
-    w_map = defaultdict(int)
-    with open(filename, 'r') as f:
-        for line in f:
-            words = line.split()
-            for word in words:
-                w_map[word] += 1
-    counter = 0
-    pre_count = 0
-    for k, v in sorted(w_map.items(), key=lambda i:i[1], reverse=True):
-        counter += 1
-        if counter <= 3 or v == pre_count:
-            print('{}: {}'.format(k, v))
-            pre_count = v
-    return
 
-top_three(sys.argv[1])
+def top_ten_words(file):
+  filtered_words = ['if', 'the', 'and', 'i', 'is', 'an', 'has']
+  puncuation = ['.', ',', '?', '!']
+  word_count = {}
+  word_count_list = []
+
+  with open(file, 'r') as in_file:
+    for line in in_file:
+      words = line.split()
+      for word in words:
+        first_char = word[0]
+        last_char = word[-1]
+
+        if first_char in puncuation:
+          stripped_word = word.lstrip(first_char)
+          if stripped_word.lower() in filtered_words:
+            pass
+          elif stripped_word not in word_count:
+            word_count[stripped_word] = 1
+          else:
+            word_count[stripped_word] += 1
+
+        elif last_char in puncuation:
+          stripped_word = word.rstrip(last_char)
+
+          if stripped_word.lower() in filtered_words:
+            pass
+          elif stripped_word not in word_count:
+            word_count[stripped_word] = 1
+          else:
+            word_count[stripped_word] += 1
+
+        else:
+          if word.lower() in filtered_words:
+            pass
+          elif word not in word_count:
+            word_count[word] = 1
+          else:
+            word_count[word] += 1
+
+  for word, count in word_count.items():
+    word_count_list.append([word, count])
+
+  top_ten = sorted(
+      word_count_list, key=lambda x: x[1], reverse=True)[:11]
+
+  for word, count in top_ten:
+    print(f'{word} -> {count}')
+
+
+def main():
+  try:
+    file = sys.argv[1]
+    top_ten_words(file)
+  except IndexError as e:
+    raise e
+
+
+if __name__ == '__main__':
+  main()
