f45c76e150341fd4eb620e9023c7c026c6f30222
Devin Duclayan <dduclayan@gmail.com>
2021-08-21 12:26:09 -0500

    Update main.py

--- most_visited_websites/main.py
+++ most_visited_websites/main.py
@@ -1,32 +1,47 @@
+"""TODO(dduclayan): DO NOT SUBMIT without one-line documentation for most_visited_websites.
+
+plan:
+
+1. split() the line so we can over each website 1 by 1
+2. add the websites to dict and increment by 1
+3. sorted() the dict
+4. output first 5 key/values to file
+
+TODO(dduclayan): DO NOT SUBMIT without a detailed description of most_visited_websites.
 """
-idea:
-1) read each line and store each website as key  into a dict
-2) if website is in dict, +1 to value
-3) if webiste is not in dict, set value to 0
-4) turn dict into list of (website, count) tuples
-5) sort list by count
-6) return first 5 websites
+import sys
 
-Complexity: O(N^2) where N is num of words inside the file
-"""
-from absl import app
 
-def main(argv):
-    print(top_websites(argv[1]))
-    return
+def most_visited_websites(file):
+  file_lines = []
+  website_count = {}
+  top_five_website_list = []
 
-def top_websites(file):
-    dic = {}
-    with open(file, 'r') as fd:
-        for line in fd:
-            sites = line.split()[1:]
-            for site in sites:
-                if site not in dic:
-                    dic[site] = 0
-                dic[site] += 1
-    tuples = [(k, v) for k, v in dic.items()]
-    ranks = sorted(tuples, key=lambda c: c[1], reverse=True)
-    return [s for s,_ in ranks[:5]]
+  with open(file, 'r') as in_file:
+    for line in in_file:
+      file_lines.append(line)
+
+  for word in file_lines:
+    words = word.split()
+    for wo in words[1:]:  # O(n^2)
+      if wo in website_count:
+        website_count[wo] += 1
+      else:
+        website_count[wo] = 1
+
+  sorted_websites = sorted(
+      website_count.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)[:5]
+
+  print('sorted_websites: ', sorted_websites)
+
+  for x, y in sorted_websites:
+    print(f'{x} {y}')
+
+
+def main():
+  file = sys.argv[1]
+  most_visited_websites(file)
+
 
 if __name__ == '__main__':
-    app.run(main)
+  main()
