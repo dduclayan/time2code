741dc4cae15c150bc2164b2408189f9572be6317
Devin Duclayan <dduclayan@gmail.com>
2021-08-27 23:26:19 -0500

    Create dedup_large_file.py

---
+++ dedup_large_file.py
@@ -0,0 +1,44 @@
+"""
+
+There are so many duplicated lines and you will need to remove them and return a
+new file that only contains unique lines.
+Assume all unique lines will fit in memory.
+
+plan:
+1. make a generator function to yield lines of the file
+2. add lines to set()
+-- set() only has unique values
+3. write out to file 
+
+questions:
+1. Do I need to sort lines?
+
+time complexity: O(n) for number of lines in file 
+"""
+
+import sys
+
+
+def generator(file):
+  with open(file) as in_file:
+    for line in in_file:
+      yield line
+
+
+def dedup_large_file():
+  file = sys.argv[1]
+  lines = set()
+
+  for line in generator(file):
+    lines.add(line)
+
+  with open('dedup_out.txt', 'w') as out_file:
+    out_file.writelines(lines)
+
+    
+def main():
+  dedup_large_file()
+
+
+if __name__ == '__main__':
+  main()
